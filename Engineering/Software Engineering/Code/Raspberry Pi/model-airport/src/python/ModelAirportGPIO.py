d131ce317d02364d22794f1d5d4e66d598baec8e
Darshan Shet <49994181+DarshanShet777@users.noreply.github.com>
2020-03-25 13:48:03 -0500

    Finished, tested, and improved gpio code with subtle improvements

--- Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/ModelAirportGPIO.py
+++ Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/ModelAirportGPIO.py
@@ -4,9 +4,8 @@ from time import sleep
 
 class ModelAirportGPIO:
     def __init__(self):
-        f = open("/media/pi/9484-DE4D/model-airport/res/ModelAirportGPIO.txt", "r")
-        s = f.read()
-        self.layout = ast.literal_eval(s)
+        f = open("/media/pi/9484-DE4D/model-airport/res/ModelAirportGPIO.txt", "r").read()
+        self.layout = ast.literal_eval(f)
         tempInputPins = []
         tempOutputPins = []
         for y in self.layout:
@@ -16,15 +15,18 @@ class ModelAirportGPIO:
                 tempOutputPins.append(self.layout[y]['Output_GPIO_Pin']['Pin'])
         inputPins = tuple(tempInputPins)
         outputPins = tuple(tempOutputPins)
-        #print(inputPins)
-        #print(outputPins)
+        print(inputPins)
+        print(outputPins)
         self.input = ButtonBoard(*inputPins)
         self.output = LEDBoard(*outputPins)
     def get_Device_State(self, device):
         if device["object"] in self.layout:
             if "Input_GPIO_Pin" in self.layout[device["object"]]:
-                #self.input.value(self.layout[device["object"]]["Input_GPIO_Pin"]["Index"])
-                pass
+                val = self.input[self.layout[device["object"]]["Input_GPIO_Pin"]["Index"]].is_pressed
+                packedVal = {'state': val}
+                return packedVal
+            else:
+                return {'state':False, 'message':'Not an input device'}
     def set_Device_State(self, device):
         if device["object"] in self.layout:
             if device["state"] == True:
@@ -34,7 +36,13 @@ class ModelAirportGPIO:
                 if "Output_GPIO_Pin" in self.layout[device["object"]]:
                     self.output.off(self.layout[device["object"]]["Output_GPIO_Pin"]["Index"])
 
+#Testing done with LED on pin 17 and button on pin 23
 test = ModelAirportGPIO()
 test.set_Device_State({'object':'Pavement-Lighting', 'state': True})
-sleep(5)
+sleep(3)
 test.set_Device_State({'object':'Pavement-Lighting', 'state':False})
+myVal = test.get_Device_State({'object': 'Backstage-Exit'})
+print(myVal)
+sleep(3)
+myVal = test.get_Device_State({'object': 'Backstage-Exit'})
+print(myVal)
