2197b392153b67742d55631afbbb4f20790af17b
Darshan Shet <49994181+DarshanShet777@users.noreply.github.com>
2020-03-25 14:40:42 -0500

    Removed error checking to run faster

--- Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/ModelAirportGPIO.py
+++ Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/ModelAirportGPIO.py
@@ -15,27 +15,18 @@ class ModelAirportGPIO:
                 tempOutputPins.append(self.layout[y]['Output_GPIO_Pin']['Pin'])
         inputPins = tuple(tempInputPins)
         outputPins = tuple(tempOutputPins)
-        print(inputPins)
-        print(outputPins)
         self.input = ButtonBoard(*inputPins)
         self.output = LEDBoard(*outputPins)
     def get_Device_State(self, device):
-        if device["object"] in self.layout:
-            if "Input_GPIO_Pin" in self.layout[device["object"]]:
-                val = self.input[self.layout[device["object"]]["Input_GPIO_Pin"]["Index"]].is_pressed
-                packedVal = {'state': val}
-                return packedVal
-            else:
-                return {'state':False, 'message':'Not an input device'}
+        val = self.input[self.layout[device["object"]]["Input_GPIO_Pin"]["Index"]].is_pressed
+        packedVal = {'state': val}
+        return packedVal
     def set_Device_State(self, device):
-        if device["object"] in self.layout:
             if device["state"] == True:
-                if "Output_GPIO_Pin" in self.layout[device["object"]]:
-                    self.output.on(self.layout[device["object"]]["Output_GPIO_Pin"]["Index"])
+                self.output.on(self.layout[device["object"]]["Output_GPIO_Pin"]["Index"])
             else:
-                if "Output_GPIO_Pin" in self.layout[device["object"]]:
-                    self.output.off(self.layout[device["object"]]["Output_GPIO_Pin"]["Index"])
-
+                self.output.off(self.layout[device["object"]]["Output_GPIO_Pin"]["Index"])
+'''
 #Testing done with LED on pin 17 and button on pin 23
 test = ModelAirportGPIO()
 test.set_Device_State({'object':'Pavement-Lighting', 'state': True})
@@ -46,3 +37,4 @@ print(myVal)
 sleep(3)
 myVal = test.get_Device_State({'object': 'Backstage-Exit'})
 print(myVal)
+'''
