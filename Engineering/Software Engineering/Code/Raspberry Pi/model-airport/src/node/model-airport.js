db97723340ad2b263063e3bca08bb5b5f777d7fb
Darshan Shet <49994181+DarshanShet777@users.noreply.github.com>
2020-06-18 12:05:05 -0500

    Completed and tested QuickConnect.py

--- Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/node/model-airport.js
+++ Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/node/model-airport.js
@@ -10,7 +10,17 @@ let pyshell = new PythonShell('../python/QuickConnect.py', options);
 pyshell.on('message', function(message){
 	console.log(message);
 	if(message.includes("[Input]")){
-		pyshell.send("N");
+		if(message.includes("Ready?")){
+			pyshell.send("Y");
+		}
+	}
+	
+	//Code below tests model airport software
+	if(message.includes("ATC Requesting Departure;Type \"SA202 Runway 09 Line Up And Wait\"")){
+		pyshell.send("SA202 Runway 09 Line Up And Wait");
+	}
+	if(message.includes("ATC Runway 09 Line Up And Wait;Type \"SA202 Cleared For Takeoff\"")){
+		pyshell.send("SA202 Cleared For Takeoff");
 	}
 	
 });
--- Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/QuickConnect.py
+++ Engineering/Software Engineering/Code/Raspberry Pi/model-airport/src/python/QuickConnect.py
@@ -20,7 +20,6 @@ shutOffTime = currentTime + timedelta(seconds = 30)
 
 # Determine if caller is ready to run the airport
 print("[QuickConnect][Input] Ready?")
-#isReady = stdin.readline()
 isReady = input()
 
 if(isReady != "Y"):
@@ -32,15 +31,29 @@ if(isReady != "Y"):
     sys.exit(0)
 
 # Run model airport until end time has been reached
+# Get input from caller and verify
+# If command is legit, run the associated operation
+
 while currentTime < shutOffTime:
-    # Activate the runway and allow robot to move
+    # Initiate airport operation by printing request message to user
+    print("ATC Requesting Departure;Type \"SA202 Runway 09 Line Up And Wait\"")
+    # Activate the runway and allow robot to move when user enters the right input
+    getCommand = input()
+    while(getCommand != "SA202 Runway 09 Line Up And Wait"):
+        getCommand = input()
     gpio.setDeviceState("Backstage-Entrance", False)
     gpio.setDeviceState("Runway-Threshold", True)
-    sleep(20)
+    sleep(3)
+    
     # Activate the backstage and allow robot to move
+    print("ATC Runway 09 Line Up And Wait;Type \"SA202 Cleared For Takeoff\"")
+    getCommand = input()
+    while(getCommand != "SA202 Cleared For Takeoff"):
+        getCommand = input()
+    print("ATC Cleared For Takeoff;Please Wait...")
     gpio.setDeviceState("Runway-Threshold", False)
     gpio.setDeviceState("Backstage-Entrance", True)
-    sleep(20)
+    sleep(3)
     # Update time so loop exits correctly
     currentTime = datetime.now()
 
