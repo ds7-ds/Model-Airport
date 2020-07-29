a82e67a5a17c7b3aa2bc1262c0df7bd2e019165f
Darshan Shet <49994181+DarshanShet777@users.noreply.github.com>
2020-07-29 16:00:04 -0500

    Testing logging in bash script

--- Engineering/Software Engineering/Code/Raspberry Pi/model-airport/test/bash/model-airport.sh
+++ Engineering/Software Engineering/Code/Raspberry Pi/model-airport/test/bash/model-airport.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 
-read -t 2 -p "Waiting for desktop to load..."
-echo ""
-
-
 echo "Logging bash output to file..."
 flashDriveID=E89D-1133
 logFilePath=/media/pi/$flashDriveID/model-airport/logs
 echo $(date) >> log.txt
-cp log.txt $logFilePath
+
+
+read -t 2 -p "Waiting for desktop to load..."
+echo ""
+echo "Waiting for desktop to load..." >> log.txt
+
 
 echo "Checking messaging proxy server status..."
+echo "Checking messaging proxy server status..." >> log.txt
 if wget -q --spider https://model-airport.herokuapp.com
 then
 	echo ONLINE
+	echo ONLINE >> log.txt
 else
 	echo OFFLINE
+	echo OFFLINE >> log.txt
+	echo "Shutting down in 30 seconds..." >> log.txt
+	cp log.txt $logFilePath
 	read -t 30 -p "Shutting down in 30 seconds..."
 	sudo shutdown -h now	
 fi
@@ -26,14 +32,19 @@ echo "Checking webcam proxy server status..."
 if wget -q --spider https://whispering-sea-51322.herokuapp.com
 then
 	echo ONLINE
+	echo ONLINE >> log.txt
 else
 	echo OFFLINE
+	echo OFFLINE >> log.txt
+	echo "Shutting down in 30 seconds..." >> log.txt
+	cp log.txt $logFilePath
 	read -t 30 -p "Shutting down in 30 seconds..."
 	sudo shutdown -h now
 fi
 
 
 echo "Finding model airport control and webcam software..."
+echo "Finding model airport control and webcam software..." >> log.txt
 : '
 If flash drive changes, fix the ID by looking it up on CMD using this path ../../media/pi/FLASH-DRIVE-ID
 '
@@ -42,32 +53,44 @@ webcamSoftwareFilePath=/media/pi/$flashDriveID/model-airport/src/node/model-airp
 if [ -d $controlSoftwareFilePath ]
 then
 	echo "Directory found..."
+	echo "Directory found..." >> log.txt
 	cd $controlSoftwareFilePath
 	if [ -f model-airport.js ]
 	then
 		echo "File found..."
+		echo "File found..." >> log.txt
 	else
 		echo "File not found..."
+		echo "File not found..." >> log.txt
+		echo "Shutting down in 30 seconds..." >> log.txt
+	  cp log.txt $logFilePath
 		read -t 30 -p "Shutting down in 30 seconds..."
 		sudo shutdown -h now
 	fi
 else
 	echo "Directory not found..."
+	echo "Directory not found..." >> log.txt
+	echo "Shutting down in 30 seconds..." >> log.txt
+  cp log.txt $logFilePath
 	read -t 30 -p "Shutting down in 30 seconds..."
 	sudo shutdown -h now
 fi
 
 
 echo "Determining start-up and shut-down times..."
+echo "Determining start-up and shut-down times..." >> log.txt
 startTime=$(date)
 startTimeInt=$(date +%s)
 endTime=$(date --date='35 minutes')
 endTimeInt=$(date --date='35 minutes' +%s)
 echo $startTime
+echo $startTime >> log.txt
 echo $endTime
+echo $endTime >> log.txt
 
 
 echo "Running software until shut-down time..."
+echo "Running software until shut-down time..." >> log.txt
 runTimeInt=$(date +%s)
 while [ $runTimeInt -le $endTimeInt ]
 do
@@ -85,6 +108,8 @@ do
 done
 
 
+echo "Shutting down software and RPi..." >> log.txt
+cp log.txt $logFilePath
 read -t 10 -p "Shutting down software and RPi..."
 : '
 	stackoverflow.com/questions/21470362/find-the-pids-of-running-processes-and-store-as-an-array
---
+++ Engineering/Software Engineering/Test Labs/Test Lab 6/Test Lab 6 - Bash Logging.md
@@ -0,0 +1,13 @@
+# Test Lab 5 - Bash Logging
+
+## Required
+
+* Bash script file
+
+## Instructions
+
+1. Log bash script wherever descriptive echo statements are used and copy file to flash drive when shutdown is near
+
+## Results
+
+> 
